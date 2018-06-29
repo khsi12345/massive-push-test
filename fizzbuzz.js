c77178377c7b41872879e25f3c85172f04a3a0b4
cheonmro <cheonmro@gmail.com>
2018-06-29 16:43:54 +0900

    Add fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,12 @@
-console.log('hello');
+function fizzbuzz(int) {
+  if(int % 15 === 0) {
+    return 'fizzbuzz';
+  } else if(int % 3 === 0) {
+    return 'fizz';
+  } else if(int % 5 === 0) {
+    return 'buzz';
+  } else {
+    return 'error';
+  }
+}
+console.log(fizzbuzz(10));
